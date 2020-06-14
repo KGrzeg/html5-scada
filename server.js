1076802cd432adb0e11d48d5575498132699ec91
KGrzeg <kupczykgrzeg@gmail.com>
2020-06-14 19:16:07 +0200

    Serve static files

---
+++ server.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+
+const express = require("express");
+
+const app = express();
+const port = 3000;
+
+app.use(express.static("public"));
+
+app.listen(port, () =>
+  console.log(`Server running at http://localhost:${port}`)
+);
