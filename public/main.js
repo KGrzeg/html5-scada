9b6f27d567cff6b206c23c19ac2fa00f17155cf0
KGrzeg <kupczykgrzeg@gmail.com>
2020-06-14 21:04:13 +0200

    General Vue Binding

--- public/main.js
+++ public/main.js
@@ -1,8 +1,25 @@
+const messageTime = 3000;
+
 const app = new Vue({
   el: "#app",
   data: {
     connected: false,
-    message: "hello",
+    connecting: false,
+    messagesStack: [],
+    controller: {
+      set: {
+        u: 0,
+        Kp: 1,
+        Ts: 0,
+        Ti: 0,
+      },
+      view: {
+        u: 0,
+        Kp: 1,
+        Ts: 0,
+        Ti: 0,
+      },
+    },
   },
   computed: {
     status() {
@@ -11,10 +28,47 @@ const app = new Vue({
     btnConnectTitle() {
       return this.connected ? "Disconnect" : "Connect";
     },
+    message() {
+      if (this.messagesStack.length == 0) return "";
+
+      return this.messagesStack[0];
+    },
   },
   methods: {
-    connectCb() {
-      this.connected = !this.connected;
+    connect() {
+      if (!this.connected) {
+        //TODO ws connection
+        this.connecting = true;
+
+        setTimeout(() => {
+          this.connecting = false;
+          this.connected = true;
+          this.pushMessage("Connected");
+        }, 1000);
+      } else {
+        this.connected = !this.connected;
+        this.pushMessage("Disconnected");
+      }
+    },
+    submitU() {
+      this.controller.set.u = parseFloat(this.controller.view.u);
+      this.sendInputs();
+    },
+    submitPID() {
+      this.controller.set.Kp = parseFloat(this.controller.view.Kp);
+      this.controller.set.Ti = parseFloat(this.controller.view.Ti);
+      this.controller.set.Ts = parseFloat(this.controller.view.Ts);
+      this.sendInputs();
+    },
+    pushMessage(text) {
+      this.messagesStack.push(text);
+      setTimeout(() => {
+        this.messagesStack = this.messagesStack.slice(1);
+        console.log("hehe");
+      }, messageTime);
+    },
+    sendInputs() {
+      //send packet via ws
     },
   },
 });
